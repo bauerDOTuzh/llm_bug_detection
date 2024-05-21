67f6a2091012e6d33db7da66e297929db212f5df
bauerDOTuzh <91192620+bauerDOTuzh@users.noreply.github.com>
2024-05-21 17:31:01 +0200

    php refractored for content

--- RQ2_nith copy/source_files/php/3968/3968_smallestBuggy.php
+++ RQ2_nith copy/source_files/php/3968/3968_smallestBuggy.php
@@ -1,12 +1,11 @@
 <?php
 {prepend_content}
-protected function afterDisplay(&$response, &$model, &$params) {
+protected function get_response_file_data(&$model, &$response) {
 
     $response['data']['path'] = $model->path;
     $response['data']['size'] = $model->fsFile->size();
     $response['data']['extension'] = strtolower($model->fsFile->extension());
     $response['data']['handler']='startjs:function(){'.$model->getDefaultHandler()->getHandler($model).'}:endjs';
 
-    return parent::afterDisplay($response, $model, $params);
 }
 {append_content}
